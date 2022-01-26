b57ee49f6ef28c0c0f7f421d6c669292e9440103
Roman Steiner <roman@toastlab.ch>
2022-01-26 12:50:53 +0000

    fix dragging to columns that don't match constraints

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ panel.plugin("rasteiner/conditionalblocks", {
       const Layout = BlockLayouts.components["k-layout"];
       const LayoutColumn = Layout.components["k-layout-column"];
       const BlockSelector = Vue.component("k-block-selector").options;
+      const Blocks = Vue.component("k-blocks").options;
 
       Vue.component("k-layout-field", {
         extends: LayoutField,
@@ -24,21 +25,50 @@ panel.plugin("rasteiner/conditionalblocks", {
       });
 
       LayoutColumn.provide = function () {
+        const [num, denum] = this.width.split("/");
+        const cwidth = parseInt(num) / parseInt(denum);
+
         return {
-          width: this.width,
+          cwidth,
         };
       };
 
-      BlockSelector.inject = ["constraints", "width"];
+      const draggableOptions = Blocks.computed.draggableOptions;
+      delete Blocks.computed.draggableOptions;
+
+      Vue.component("k-blocks", {
+        extends: Blocks,
+        inject: ["constraints", "cwidth"],
+        computed: {
+          draggableOptions() {
+            const original = draggableOptions.call(this);
+
+            if (this.constraints && this.cwidth) {
+              //remove fieldsets that are not allowed by constraints
+              original.data.fieldsets = Object.fromEntries(
+                Object.entries(original.data.fieldsets).filter(([type]) => {
+                  const constraint = this.constraints[type];
+                  return !(
+                    (constraint?.min && this.cwidth < constraint.min) ||
+                    (constraint?.max && this.cwidth > constraint.max)
+                  );
+                })
+              );
+            }
+
+            return original;
+          },
+        },
+      });
+
+      BlockSelector.inject = ["constraints", "cwidth"];
 
       const open = BlockSelector.methods.open;
       BlockSelector.methods.open = function () {
         open.call(this, ...arguments);
 
-        if (!this.constraints || !this.width) return;
-
-        const [num, denum] = this.width.split("/");
-        const width = parseInt(num) / parseInt(denum);
+        if (!this.constraints || !this.cwidth) return;
+        const width = this.cwidth;
 
         const myDisabled = Object.entries(this.constraints)
           .filter(([_, value]) => {
